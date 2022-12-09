781382a2f9c6bb6b6f1ce922762dd358705485fe
ayberkuckun <44614567+ayberkuckun@users.noreply.github.com>
2022-12-09 02:01:16 +0100

    last

--- README.md
+++ README.md
@@ -1,1 +1,25 @@
 # Serverless-Whisper-ASR
+
+**Feature Engineering Pipeline:**
+- ``feature_engineering.ipynb``
+
+We separate the feature engineering from the training since it doesn't require the precious GPU resource.
+We process the ``mozilla-foundation/common_voice_11_0`` Swedish data in order to fine-tune ``openai/whisper-small``
+We change the sampling rate of the audio first. Then, process the audio and the transcription labels 
+using feature extractor and tokenizer. Lastly we save the processed data to our drive.
+
+PS: These notebooks have been prepared to be run on Colab.
+
+**Training Pipeline:**
+- ``training.ipynb``
+
+We download the processed data form our drive and the raw model from HuggingFace.
+The final model is trained for 2000 steps with the best parameters obtained from the hyperparameter search 
+for 300 steps. Hyperparameter search has been done using Wandb and the results can be found under 
+``parameter_search.pdf``. Lastly the final model has been pushed to the HuggingFace
+
+**Inference Program (Hugging Face Space):**
+- https://huggingface.co/spaces/ayberkuckun/whisper-small-sv-SE
+
+Users can give a YouTube link to a Swedish video in order to obtain the audio and its trascribtion from our model.
+Also, the WER scores of ``base``, ``2K`` and ``2K-fine-tuned`` model can be seen in the page.
---
+++ app.py
@@ -0,0 +1,48 @@
+from transformers import pipeline
+from pytube import YouTube
+
+import gradio as gr
+import librosa
+
+import hopsworks
+
+project = hopsworks.login()
+fs = project.get_feature_store()
+
+dataset_api = project.get_dataset_api()
+
+dataset_api.download("Resources/titanic/images/latest_titanic.png", overwrite=True)  # change link
+
+pipe = pipeline(model="ayberkuckun/whisper-small-sv-SE", task="automatic-speech-recognition", chunk_length_s=30)
+
+
+def transcribe(url):
+    selected_video = YouTube(url)
+
+    try:
+        audio = selected_video.streams.filter(only_audio=True)[0]
+    except:
+        raise Exception("Can't find an mp4 audio.")
+
+    audio.download(filename="audio.mp3")
+
+    speech_array, _ = librosa.load("audio.mp3", sr=16000)
+
+    output = pipe(speech_array)
+
+    return "audio.mp3", output["text"], "latest_titanic.png"
+
+
+iface = gr.Interface(
+    fn=transcribe,
+    inputs=gr.Textbox("https://www.youtube.com/watch?v=n9g12Xm9UJM", label="Paste a YouTube video URL"),
+    outputs=[gr.Audio(label="Transcripted Audio"),
+             gr.Textbox(label="Transcription"),
+             gr.Image(label="Model Scores")
+],
+    title="Whisper Small Swedish",
+    description="Realtime demo for Swedish speech recognition using a fine-tuned Whisper small model.",
+    allow_flagging="never"
+)
+
+iface.launch()
